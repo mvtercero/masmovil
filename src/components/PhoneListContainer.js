4502bad8cc4ca49769cda393d264df72db36c06f
María Vedia <mvtercero85@gmail.com>
2018-09-16 12:02:09 +0200

    Recuperated structure PhoneListContainer

---
+++ src/components/PhoneListContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import { connect } from "react-redux";
+// import * as actionCreators from "../actions/index.js";
+
+// const mapStateToProps = state => {
+//   return {
+//     phones: state.phones
+//   }
+// }
+
+class PhoneListContainer extends React.Component {
+  // componentWillMount() {
+  //   console.log(this)
+  //   this.props.loadApi()
+  // }
+
+  render() {
+    return (
+      <div></div>
+      // <ul data={this.props.phones}>
+      //   {this.props.phones.map(phone =>
+      //     <li key={phone.id}>
+      //       {phone.title}
+      //     </li>
+      //   )}
+      // </ul>
+    )
+  }
+}
