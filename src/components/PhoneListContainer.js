ee8009acaacc022a8f0a811df82caacebd5ee43b
María Vedia <mvtercero85@gmail.com>
2018-09-16 13:34:48 +0200

    Added description, price, color and url image to PhoneListContainer

--- src/components/PhoneListContainer.js
+++ src/components/PhoneListContainer.js
@@ -19,7 +19,11 @@ class PhoneListContainer extends React.Component {
       <ul data={this.props.phones}>
         {this.props.phones.map(phone =>
           <li key={phone.id}>
-            {phone.title}
+            <h2>{phone.title}</h2>
+            <h3>{phone.description}</h3>
+            <p>{`precio: ` + phone.price}</p>
+            <p>{phone.color}</p>
+            <img src={phone.url_image} alt={phone.title} />
           </li>
         )}
       </ul>
