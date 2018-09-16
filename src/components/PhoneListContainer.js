01ef8c37c179f568cf570f18f8c31fdeb3f8c83e
María Vedia <mvtercero85@gmail.com>
2018-09-16 12:35:15 +0200

    Added Provider and createStore to index

--- src/components/PhoneListContainer.js
+++ src/components/PhoneListContainer.js
@@ -1,29 +1,28 @@
 import React from "react";
-// import { connect } from "react-redux";
-// import * as actionCreators from "../actions/index-actions.js";
+import { connect } from "react-redux";
+import * as actionCreators from "../actions/index-actions.js";
 
-// const mapStateToProps = state => {
-//   return {
-//     phones: state.phones
-//   }
-// }
+const mapStateToProps = state => {
+  return {
+    phones: state.phones
+  }
+}
 
 class PhoneListContainer extends React.Component {
-  // componentWillMount() {
-  //   console.log(this)
-  //   this.props.loadApi()
-  // }
+  componentWillMount() {
+    console.log(this)
+    this.props.loadApi()
+  }
 
   render() {
     return (
-      <div></div>
-      // <ul data={this.props.phones}>
-      //   {this.props.phones.map(phone =>
-      //     <li key={phone.id}>
-      //       {phone.title}
-      //     </li>
-      //   )}
-      // </ul>
+      <ul data={this.props.phones}>
+        {this.props.phones.map(phone =>
+          <li key={phone.id}>
+            {phone.title}
+          </li>
+        )}
+      </ul>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PhoneListContainer from "./components/PhoneListContainer.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+// import App from "./components/App.js"
 import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index-reducers.js';
+import PhoneListContainer from "./components/PhoneListContainer.js";
+
+
+let store = createStore(reducers, applyMiddleware(thunk));
 
 class App extends React.Component {
   render() {
@@ -11,6 +19,10 @@ class App extends React.Component {
   }
 }
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
