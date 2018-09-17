160aa3766ef78256d95378d548a8d3507e80d8ec
María Vedia <mvtercero85@gmail.com>
2018-09-17 17:52:35 +0200

    Created Spinner css

--- src/components/PhoneListContainer.js
+++ src/components/PhoneListContainer.js
@@ -27,7 +27,7 @@ class PhoneListContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className>
         {this.props.spinner ? <Spinner /> : null}
 
         <ul className="phone__container--list">
