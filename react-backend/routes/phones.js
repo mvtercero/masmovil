d750e73b6ef08553ea00a62425a5d80a25805ef3
María Vedia <mvtercero85@gmail.com>
2018-09-17 16:59:06 +0200

    Added more information to the API

--- react-backend/routes/phones.js
+++ react-backend/routes/phones.js
@@ -24,14 +24,38 @@ router.get('/', function (req, res, next) {
           title: "Samsung Galaxy A6",
           price: 6 + "$",
           color: "plata, negro, oro",
-          description: "Diversión sin límites",
+          description: "Prácticamente perfecto",
           url_image: "https://www.vodafone.es/img/dynamique/LISTADO_FINANCING/b52fbdb7246f436fb50e7d9c7629d842_158x270-A6.png",
           screen: 5.2 + "''",
           camera: 13 + "MP",
           storage: 16 + "GB",
           sim: "DualSim"
         },
+        {
+          id: 3,
+          title: "Huawei P20 Pro",
+          price: 25 + "$",
+          color: "plata, negro, oro",
+          description: "A la medida de tu bolsillo",
+          url_image: "https://www.vodafone.es/img/dynamique/LISTADO_FINANCING/aafc60b06de74a22a4489829056f1c40_aafc60b06de74a22a4489829056f1c40_158x270-P20ProTwilight.png",
+          screen: 5.2 + "''",
+          camera: 13 + "MP",
+          storage: 16 + "GB",
+          sim: "DualSim"
+        },
 
+        {
+          id: 4,
+          title: "Samsung Galaxy J6",
+          price: 7.50 + "$",
+          color: "plata, negro, oro",
+          description: "Diversión sin límites",
+          url_image: "https://www.vodafone.es/img/dynamique/LISTADO_FINANCING/96f4b43c0be24eeda0f779426ab77678_158x270_Galaxy-J6.png",
+          screen: 5.2 + "''",
+          camera: 13 + "MP",
+          storage: 16 + "GB",
+          sim: "DualSim"
+        },
       ]
     }
   );
